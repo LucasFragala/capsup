d0fc1c0838086453b4929c4906ddfd9723e98119
lucas.fragala@uniara.edu.br <lucfragala@gmail.com>
2024-02-19 16:26:51 -0300

    up js

--- js/main.js
+++ js/main.js
@@ -9,16 +9,9 @@ function closeMenu(){
 }
 
 function enviarMensagemWhatsApp() {
-    // Número de telefone para o qual você deseja enviar a mensagem
-    var numeroTelefone = "+5516992924468"; // Substitua pelo seu número de telefone com o código do país
-
-    // Mensagem que você deseja enviar
+    var numeroTelefone = "+5516992924468"; 
     var mensagem = "Olá! Gostaria de entrar em contato.";
-
-    // Monta a URL do WhatsApp com o número de telefone e a mensagem
     var url = "https://api.whatsapp.com/send?phone=" + encodeURIComponent(numeroTelefone) + "&text=" + encodeURIComponent(mensagem);
-
-    // Abre a URL no navegador
     window.open(url);
 }
 
@@ -28,18 +21,14 @@ function enviarFormWhatsApp() {
     var assunto = document.getElementById("assunto").value;
     var mensagem = document.getElementById("mensagem").value;
 
-    // Número de telefone para o qual você deseja enviar a mensagem
-    var numeroTelefone = "+5516992924468"; // Substitua pelo seu número de telefone com o código do país
+    var numeroTelefone = "+5516992924468";
 
-    // Monta a mensagem com as informações do formulário
-    var mensagemWhatsApp = "Nome: " + nome + "%0A";
-    mensagemWhatsApp += "Email: " + email + "%0A";
-    mensagemWhatsApp += "Assunto: " + assunto + "%0A";
+    var mensagemWhatsApp = "Nome: " + nome + "<br>";
+    mensagemWhatsApp += "Email: " + email + "<br>";
+    mensagemWhatsApp += "Assunto: " + assunto + "<br>";
     mensagemWhatsApp += "Mensagem: " + mensagem;
 
-    // Monta a URL do WhatsApp com o número de telefone e a mensagem
     var url = "https://api.whatsapp.com/send?phone=" + encodeURIComponent(numeroTelefone) + "&text=" + encodeURIComponent(mensagemWhatsApp);
 
-    // Abre a URL no navegador
     window.open(url);
 }
